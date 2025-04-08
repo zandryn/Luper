c97a732f9d3ddfa2faa757f14733b685a7b2182a
ZANDRYN <epraudite@gmail.com>
2025-04-07 22:29:19 -0400

    Initial Commit

---
+++ Luper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Luper
+//
+//  Created by Zandryn Epan on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Luper/LuperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuperApp.swift
+//  Luper
+//
+//  Created by Zandryn Epan on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LuperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
